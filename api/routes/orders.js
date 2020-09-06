c9250eceed3e4c9d34c0920d215ee4ebaba48c00
AlbertHovhannisyan <albert.hovhannisian@gmail.com>
2020-09-06 17:23:33 +0400

    Managing orders and Populating Queries with Mongoose

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,36 +1,116 @@
 
 const { Router } = require('express');
 const router = Router();
+const mongoose = require('mongoose');
+
+const Order = require('../models/order');
+const Product = require('../models/order')
 
 router.get('/', (req, res, next) => {
-    res.status(201).json({
-        message:'Order was fetched',
-    });
+   Order.find()
+       .select('Prodcut quantity _id')
+       .populate('product', 'name')
+       .exec()
+       .then(docs => {
+           res.status(200).json({
+               count: docs.length,
+               orders: docs.map(doc => {
+                   return{
+                       _id: doc._id,
+                       product: doc.product,
+                       quantity: doc.quantity,
+                       request: {
+                           type: 'GET',
+                           url:  + doc._id
+                       }
+                   };
+               })
+           });
+       })
+       .catch(err => {
+           res.status(500).json({
+               error: err
+           });
+       });
 });
 
 router.post('/', (req, res, next) => {
-    const order = {
-        productId: req.body.productId,
-        quantity: req.body.quantity
-    };
-    res.status(201).json({
-        message:'Order was created',
-        order: order
-    });
+    Product.findById(req.body.productId)
+        .then(product => {
+            if (!product) {
+                return res.status(404).json({
+                    message: 'Product not found'
+                });
+            }
+            const order = new Order({
+                _id: mongoose.Types.ObjectId(),
+                quantity: req.body.quantity,
+                product: req.body.productId,
+            });
+            return order.save()
+                .exec()
+                .then(result => {
+                    console.log(result);
+                    res.status(201).json({
+                        message: 'Order Strored',
+                        createOrder: {
+                            _id: result._id,
+                            product: result.product,
+                            quantity: result.quantity
+                        },
+                        request: {
+                            type: 'GET',
+                            url: 'http://locallhost:3000/order' + result._id
+                        }
+                    });
+                })
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Product not found',
+                error: err
+            });
+        })
 });
 
 router.get('/:orderId', (req, res, next) => {
-    res.status(201).json({
-        message:'Order details',
-        orderId: req.params.orderId
-    });
+  Order.findById(res.params.orderId)
+      .populate('product')
+      .exec()
+      .then(order => {
+          if(!order){
+              return res.status(404).json({
+                  message: 'Order Not found'
+              })
+          }
+          res.status(200).json({
+              order: order,
+              request: {
+                  type: 'GET',
+                  url: 'http://locallhost:3000/orders/'
+              }
+          })
+              .catch(err => {
+                  res.status(500).json({
+                      error: err
+                  })
+              }) ;
+      });
 });
 
 router.delete('/:orderId', (req, res, next) => {
-    res.status(201).json({
-        message:'Order details',
-        orderId: req.params.orderId
-    });
+  Order.remove({_id: req.params.orderId })
+      .exec()
+      .then(result => {
+          message: 'Order Deleted',
+          res.status(200).json({
+              request: {
+                  type: 'POST',
+                  url: 'http://locallhost:3000/orders/',
+                  body: {productId: 'ID', quantity: 'Number'}
+              }
+          })
+      });
 });
 
 module.exports = router;
